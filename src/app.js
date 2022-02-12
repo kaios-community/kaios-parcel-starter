2e01d0f366cc4ed93b965650d2e213628cc0ee84
Garrett Downs <garredow@outlook.com>
2022-02-12 09:13:08 -0500

    add export example

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 import localforage from 'localforage';
+import { formatHelloMessage } from './helpers';
 
 const store = localforage.createInstance({
   name: 'database',
 });
 
 async function sayHelloAsync() {
-  await store.setItem('name', 'Garrett');
+  await store.setItem('name', 'Garrett333');
   const name = await store.getItem('name');
-  console.log(`Hello ${name}`);
+  console.log(formatHelloMessage(name));
 }
 
 sayHelloAsync();
---
+++ src/helpers.js
@@ -0,0 +1,7 @@
+export function formatHelloMessage(name) {
+  return `Hello ${name}`;
+}
+
+export function formatGoodbyeMessage(name) {
+  return `Goodbye ${name}`;
+}
